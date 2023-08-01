e7ef5dd9c8ffc81caaa1091fc4b2f7f232b1d4d0
Greg Charvet 黑瓜 <greg.charvet@krakentechnologies.ltd>
2023-08-01 11:01:04 +0100

    disable pylint (not used here)

--- config.lua
+++ config.lua
@@ -237,9 +237,7 @@ formatters.setup {
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   { command = "mypy", filetypes = { "python" } },
-  { command = "pylint", filetypes = { "python" },
-    extra_args = {}
-  },
+  -- { command = "pylint", filetypes = { "python" }, extra_args = {} },
   {
     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
     command = "shellcheck",
