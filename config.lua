405103d311ce4b9ee14e138fe06530bc6c5dbdd7
Greg Charvet 黑瓜 <greg.charvet@krakentechnologies.ltd>
2023-08-14 15:16:47 +0100

    lint python with flake8

--- config.lua
+++ config.lua
@@ -244,6 +244,7 @@ formatters.setup {
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   { command = "mypy", filetypes = { "python" } },
+  { command = "flake8", filetypes = { "python" } },
   -- { command = "pylint", filetypes = { "python" }, extra_args = {} },
   {
     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
